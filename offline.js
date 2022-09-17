2e655d8e9eac3b362e76784bca469ad53eb5777d
Recruit_Zer0 <tadeudosgamesOFICIAL@gmail.com>
2022-09-16 21:47:16 -0300

    upload

---
+++ offline.js
@@ -0,0 +1,15 @@
+﻿{
+	"version": 1663375615,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background.png",
+		"images/player-sheet0.png",
+		"images/espinhos-sheet0.png",
+		"images/espinhos-sheet1.png",
+		"images/espinhos-sheet2.png",
+		"images/stun-sheet0.png"
+	]
+}
